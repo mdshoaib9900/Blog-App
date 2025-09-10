690b6dbc99bc4624d9ec88d20b951712b4d7859f
mdshoaib9900 <mdshoaib6360@gmail.com>
2025-09-10 19:04:33 +0530

    updated

--- MEGA-BLOG/src/components/index.js
+++ MEGA-BLOG/src/components/index.js
@@ -3,11 +3,12 @@ import Footer from "./Footer/Footer.jsx"
 import Container from "./container/Container.jsx"
 import Logo from "./Logo.jsx"
 import LogoutBtn from "./header/LogoutBtn.jsx"
-
+import RTE from "./RTE.jsx"
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
