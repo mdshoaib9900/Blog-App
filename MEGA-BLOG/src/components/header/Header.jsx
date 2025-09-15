a0d2297c2aa7a532fb72b9f7c6e4675f80b8636e
mdshoaib9900 <mdshoaib6360@gmail.com>
2025-09-15 21:51:34 +0530

    updated

--- MEGA-BLOG/src/components/header/Header.jsx
+++ MEGA-BLOG/src/components/header/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
     { name: "Home", slug: "/", active: true },
     { name: "Login", slug: "/login", active: !authStatus },
     { name: "Signup", slug: "/signup", active: !authStatus },
-    { name: "My Posts", slug: "/all-posts", active: authStatus },
+    { name: "community blogs", slug: "/all-posts", active: authStatus },
     { name: "Add Post", slug: "/add-post", active: authStatus },
   ];
 
